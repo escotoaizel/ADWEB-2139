a35637292d4269c52d9632e770d6aeb9907212e6
escotoaizel <83365589+escotoaizel@users.noreply.github.com>
2022-03-18 12:55:02 +0800

    Update myservice.service.ts

--- Midterm Hands-on Quiz: Sharing Data Across Components/myservice.service.ts
+++ Midterm Hands-on Quiz: Sharing Data Across Components/myservice.service.ts
@@ -13,7 +13,7 @@ export class MyserviceService {
       {image: "/assets/images/avatar2.png",id:102, fname: 'James', lname: 'Atienza', email: 'jatienza@hau.edu.ph'},
       {image: "/assets/images/avatar3.png",id:103, fname: 'John', lname: 'Cena', email: 'jcena@hau.edu.ph'},
       {image: "/assets/images/avatar4.png",id:104, fname: 'Robert', lname: 'Quintana', email: 'rquintana@hau.edu.ph'},
-      {image: "/assets/images/avatar5.png",id:105, fname: 'Aizel', lname: 'Escoto', email: 'alescoto@hau.edu.ph'},
+      {image: "/assets/images/avatar5.png",id:105, fname: 'Aizel', lname: 'Escoto', email: 'alescoto@student.hau.edu.ph'},
     ]
   }
 }
